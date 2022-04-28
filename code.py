6a2ac5cffc645343159740cc0d4729bbbceff35e
xstuden <studenic.maros@gmail.com>
2022-04-28 16:41:51 +0200

    aah

--- code.py
+++ code.py
@@ -1,18 +1,18 @@
+import urllib
+
 import yaml
-import subprocess
-import hashlib
 import subprocess
 import flask
 
 
 def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=file)
+    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)
 
 
 def load_config(filename):
     # Load a configuration file into YAML
-    stream = file.open(filename, "w")
+    stream = open(filename, "w")
     config = yaml.load(stream)
 
 
@@ -29,7 +29,6 @@ def fetch_website(urllib_version, url):
     http = urllib.PoolManager()
     r = http.request('GET', url)
     return r.data
-
 
 
 @app.route("/")
